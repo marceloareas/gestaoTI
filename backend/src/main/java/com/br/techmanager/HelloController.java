444a604f57327819dc1798db22bc21bc72bda9f3
daniel <dmedeiros1209@gmail.com>
2025-10-13 21:44:26 -0300

    começando backend

---
+++ backend/src/main/java/com/br/techmanager/HelloController.java
@@ -0,0 +1,12 @@
+package com.br.techmanager;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/api/hello")
+    public String hello() {
+        return "🚀 Spring Boot está rodando!";
+    }
+}
